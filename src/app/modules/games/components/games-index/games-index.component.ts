99e88b1406ea89672e4231e456cc62265ff7ec6a
MARKMACHINE <NaymarkCK.44@gmail.com>
2023-05-14 22:48:09 +0700

    settup formJson

--- src/app/modules/games/components/games-index/games-index.component.ts
+++ src/app/modules/games/components/games-index/games-index.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Observable} from "rxjs";
-import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup} from "@angular/forms";
 import {GamesService} from "../../../../services/games.service";
 
 @Component({
@@ -10,16 +10,31 @@ import {GamesService} from "../../../../services/games.service";
   styleUrls: ['./games-index.component.sass']
 })
 export class GamesIndexComponent implements OnInit {
+  // Data Received From Resolver
   games$: Observable<object>
+  // FormControl
   formDataJson: FormGroup = this.fb.group({})
+  formStatus: boolean = false
+  // Paths
+  pathLocal: string = './assets/json/form-data-json/formGames.json'
 
   constructor(private route: ActivatedRoute, private fb: FormBuilder, private service: GamesService) {
-    // Get resolver data from route.
-    this.games$ = route.data
-    // Convert the JSON object to a new FormControl object.
-    service.getLocal('./assets/json/form-data-json/formGames.json').subscribe(dataJson => this.formDataJson = new FormGroup(
-      Object.fromEntries(Object.entries(dataJson).map(([key, value]) => [key, new FormControl(value)]))));
+    this.games$ = route.data; // Get resolver data from route.
+    service.getLocal(this.pathLocal).subscribe(dataJson => {
+      this.formDataJson = this.fb.group(dataJson[0]);
+      Object.entries(dataJson[0]).forEach(([key, value]) => {
+        (typeof value === 'object') ? (this.formDataJson.removeControl(key),
+          this.formDataJson.addControl(key, this.fb.array([this.fb.group(Object(value)[0])]))) : undefined
+      });
+      this.formDataJson.reset();
+      this.formStatus = true;
+    }); // Convert the JSON object to a new FormControl object.
   }
+
   ngOnInit(): void {
+  }
+
+  handleFormSubmit() {
+    console.log(this.formDataJson.value)
   }
 }
--- src/app/modules/games/games.module.ts
+++ src/app/modules/games/games.module.ts
@@ -8,6 +8,9 @@ import {EditGamesComponent} from './components/edit-games/edit-games.component';
 import {TrashGamesComponent} from './components/trash-games/trash-games.component';
 import {SearchGamesComponent} from './components/search-games/search-games.component';
 import {GamesIndexComponent} from './components/games-index/games-index.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { GenresComponent } from './components/add-games/genres/genres.component';
+import { DetailsComponent } from './components/add-games/details/details.component';
 
 @NgModule({
   declarations: [
@@ -17,11 +20,14 @@ import {GamesIndexComponent} from './components/games-index/games-index.componen
     TrashGamesComponent,
     SearchGamesComponent,
     GamesIndexComponent,
+    GenresComponent,
+    DetailsComponent,
 
   ],
   imports: [
     CommonModule,
     GamesRoutingModule,
+    ReactiveFormsModule,
 
   ]
 })
